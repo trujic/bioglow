bf5feba9d0a261127a8e707f1b4870fe9ae9417f
Nemanja Trujic <nemanja@factorblue.com>
2025-06-18 00:17:48 +0200

    email sending fix

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -21,7 +21,13 @@ const treatments = [
 // Validation schema
 const schema = yup.object({
   name: yup.string().required("Full name is required"),
-  phone: yup.string().required("Phone number is required"),
+  phone: yup
+    .string()
+    .required("Phone number is required")
+    .matches(
+      /^\d{9,}$/,
+      "Phone number must be at least 9 digits and contain only numbers"
+    ),
   email: yup.string().email("Invalid email").required("Email is required"),
   treatment: yup.string(),
   date: yup
@@ -72,114 +78,117 @@ async function handleSubmit(values) {
         <Form
           ref="bookingForm"
           :validation-schema="schema"
-          type="submit"
-          class="bg-[#ECE9E1] pt-6 md:pt-20 pb-24 px-4 md:px-20 flex flex-col gap-4 max-w-[550px] m-auto relative z-10 border border-black"
-          id="booking"
-          name="booking"
-          method="POST"
-          netlify
-          @submit="handleSubmit"
+          v-slot="{ handleSubmit }"
         >
-          <h4
-            class="text-[34px] md:text-[46px] text-[#2A2B30] text-center font-light pb-[24px] font-sloop tracking-[-1px] md:tracking-[2px]"
+          <form
+            @submit.prevent="handleSubmit(onSubmit)"
+            class="bg-[#ECE9E1] pt-6 md:pt-20 pb-24 px-4 md:px-20 flex flex-col gap-4 max-w-[550px] m-auto relative z-10 border border-black"
+            id="booking"
+            name="booking"
           >
-            Book an appointment
-          </h4>
-          <div class="flex flex-col">
-            <label
-              class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
+            <h4
+              class="text-[34px] md:text-[46px] text-[#2A2B30] text-center font-light pb-[24px] font-sloop tracking-[-1px] md:tracking-[2px]"
             >
-              Full name
-            </label>
-            <Field
-              name="name"
-              class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
-            />
-            <ErrorMessage name="name" class="text-red-600 text-sm" />
-          </div>
+              Book an appointment
+            </h4>
+            <div class="flex flex-col">
+              <label
+                class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
+              >
+                Full name
+              </label>
+              <Field
+                name="name"
+                class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
+              />
+              <ErrorMessage name="name" class="text-red-600 text-sm" />
+            </div>
 
-          <div class="flex flex-col">
-            <label
-              class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
-            >
-              Phone
-            </label>
-            <Field
-              name="phone"
-              class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
-            />
-            <ErrorMessage name="phone" class="text-red-600 text-sm" />
-          </div>
+            <div class="flex flex-col">
+              <label
+                class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
+              >
+                Phone
+              </label>
+              <Field
+                name="phone"
+                class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
+              />
+              <ErrorMessage name="phone" class="text-red-600 text-sm" />
+            </div>
 
-          <div class="flex flex-col">
-            <label
-              class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
-            >
-              Email
-            </label>
-            <Field
-              name="email"
-              class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
-            />
-            <ErrorMessage name="email" class="text-red-600 text-sm" />
-          </div>
+            <div class="flex flex-col">
+              <label
+                class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
+              >
+                Email
+              </label>
+              <Field
+                name="email"
+                class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
+              />
+              <ErrorMessage name="email" class="text-red-600 text-sm" />
+            </div>
 
-          <div class="flex flex-col">
-            <label
-              class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
-            >
-              Select a treatment
-            </label>
-            <Field
-              as="select"
-              name="treatment"
-              class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
-            >
-              <option value="" disabled>Select a treatment</option>
-              <option v-for="t in treatments" :key="t" :value="t">
-                {{ t }}
-              </option>
-            </Field>
-            <ErrorMessage name="treatment" class="text-red-600 text-sm" />
-          </div>
+            <div class="flex flex-col">
+              <label
+                class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
+              >
+                Select a treatment
+              </label>
+              <Field
+                as="select"
+                name="treatment"
+                class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base hover:cursor-pointer"
+              >
+                <option value="" disabled>Select a treatment</option>
+                <option v-for="t in treatments" :key="t" :value="t">
+                  {{ t }}
+                </option>
+              </Field>
+              <ErrorMessage name="treatment" class="text-red-600 text-sm" />
+            </div>
 
-          <div class="flex flex-col">
-            <label
-              class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px] w-full"
-            >
-              Select a date
-            </label>
-            <Field
-              name="date"
-              type="date"
-              :min="new Date(Date.now() + 86400000).toISOString().split('T')[0]"
-              class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
-            />
-            <ErrorMessage name="date" class="text-red-600 text-sm" />
-          </div>
+            <div class="flex flex-col">
+              <label
+                class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px] w-full"
+              >
+                Select a date
+              </label>
+              <Field
+                name="date"
+                type="date"
+                :min="
+                  new Date(Date.now() + 86400000).toISOString().split('T')[0]
+                "
+                class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
+              />
+              <ErrorMessage name="date" class="text-red-600 text-sm" />
+            </div>
 
-          <div class="flex flex-col">
-            <label
-              class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
-            >
-              Message
-            </label>
-            <Field
-              as="textarea"
-              name="message"
-              class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
-            />
-            <ErrorMessage name="message" class="text-red-600 text-sm" />
-          </div>
+            <div class="flex flex-col">
+              <label
+                class="uppercase font-light font-helveticaDisplay text-xs md:text-base tracking-[1px]"
+              >
+                Message
+              </label>
+              <Field
+                as="textarea"
+                name="message"
+                class="bg-[#ECE9E1] border-b border-black outline-none text-base md:text-base"
+              />
+              <ErrorMessage name="message" class="text-red-600 text-sm" />
+            </div>
 
-          <div class="absolute bottom-4 right-10">
-            <button
-              type="submit"
-              class="max-w-fit border-b border-black md:p-2 px-4 font-light text-[26px] md:text-[40px] font-sloop"
-            >
-              Send
-            </button>
-          </div>
+            <div class="absolute bottom-4 right-10">
+              <button
+                type="submit"
+                class="max-w-fit border-b border-black md:p-2 px-4 font-light text-[26px] md:text-[40px] font-sloop"
+              >
+                Send
+              </button>
+            </div>
+          </form>
         </Form>
       </div>
     </div>
